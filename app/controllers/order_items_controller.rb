9c66761b94874f0950ea80111682cec4c5e24881
tatiana.groiss@users.noreply.github.com <tatiana.groiss@gmail.com>
2025-11-04 11:12:48 +0100

    actions in the controller OrderItems are defined

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,2 +1,24 @@
 class OrderItemsController < ApplicationController
+  def index
+    @order_items = OrderItem.all
+  end
+
+  def show
+    @order_item = OrderItem.find(params[:id])
+  end
+
+  def create
+    @order_item = OrderItem.new(order_item_params)
+    if @order_item.save
+      redirect_to @order_item, notice: 'Order item was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def order_item_params
+    params.require(:order_item).permit(:user_id, :menu_item_id, :wine_id, :beverage_id, :quantity)
+  end
 end
