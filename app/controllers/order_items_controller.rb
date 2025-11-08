4b1f137d93d32e64fa11a5bd6e87de5142435130
tatiana.groiss@users.noreply.github.com <tatiana.groiss@gmail.com>
2025-11-08 21:05:27 +0100

    added black and gold on styles

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -15,8 +15,9 @@ class OrderItemsController < ApplicationController
   def create
     @order_item = OrderItem.new(order_item_params)
     @order_item.user = current_user
+
     if @order_item.save
-      redirect_to order_items_path, notice: "Item added to your order."
+      #redirect_to order_items_path, notice: "Item added to your order."
     else
       render :new, status: :unprocessable_entity
     end
@@ -25,12 +26,12 @@ class OrderItemsController < ApplicationController
   def destroy
     @order_item = OrderItem.find(params[:id])
     @order_item.destroy
-    redirect_to order_items_path, notice: "Item removed from your order."
+    #redirect_to order_items_path, notice: "Item removed from your order."
   end
 
   private
 
   def order_item_params
-    params.require(:order_item).permit(:user_id, :menu_item_id, :wine_id, :beverage_id, :quantity, :total)
+    params.require(:order_item).permit(:user, :menu_item_id, :wine_id, :beverage_id, :quantity, :total)
   end
 end
