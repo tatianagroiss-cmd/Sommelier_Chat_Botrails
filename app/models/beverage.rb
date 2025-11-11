4f33966eebf4b87b333c1983a122467faebae560
tatiana.groiss@users.noreply.github.com <tatiana.groiss@gmail.com>
2025-11-11 12:25:40 +0100

    pgvector installed

--- app/models/beverage.rb
+++ app/models/beverage.rb
@@ -1,8 +1,17 @@
 class Beverage < ApplicationRecord
   has_many :order_items
+  has_neighbors :embedding
+  after_create :set_embedding
 
   validates :name, presence: true
   validates :description, length: { maximum: 300 }
   validates :price, presence: true, numericality: { greater_than: 0 }
   validates :volume, presence: true, numericality: { greater_than: 0 }
+
+  private
+
+  def set_embedding
+    embedding = RubyLLM.embed("Product: #{name}. Description: #{description}")
+    update(embedding: embedding.vectors)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_11_06_142913) do
+ActiveRecord::Schema[7.1].define(version: 2025_11_11_101821) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "vector"
 
   create_table "beverages", force: :cascade do |t|
     t.string "category"
@@ -22,6 +23,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_11_06_142913) do
     t.string "volume"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.vector "embedding", limit: 1536
   end
 
   create_table "menu_items", force: :cascade do |t|
@@ -30,6 +32,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_11_06_142913) do
     t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.vector "embedding", limit: 1536
   end
 
   create_table "order_items", force: :cascade do |t|
@@ -68,6 +71,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_11_06_142913) do
     t.string "volume"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.vector "embedding", limit: 1536
   end
 
   add_foreign_key "order_items", "beverages"
